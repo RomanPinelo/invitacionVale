d493bb5f94efd0d0079b3ff049f76e2812a0bee8
RomanPinelo <prof.roman.pinelo@gmail.com>
2023-03-14 20:16:51 -0600

    Sección de fotos

--- script.js
+++ script.js
@@ -51,3 +51,51 @@ var typed1 = new Typed('#typedFirstMessage', {
   cursorChar: '|',
   contentType: 'html',
 });
+
+
+// ___________________________________________________________________________________________________________________________________
+// Slider
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length - 1]; //Siempre obtengo la última imagen para ponerla al principio y se vea el efecto del slider
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast); //Coloca el último elemento al inicio del slider
+
+function Next() {
+  let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);  //El primer elemento se mueve al final
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+function Prev() {
+  let sliderSection = document.querySelectorAll(".slider__section");
+  let sliderSectionLast = sliderSection[sliderSection.length - 1];
+  slider.style.marginLeft = "0";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);  //El último elemento se mueve al inicio
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+  Next();
+});
+
+btnLeft.addEventListener('click', function(){
+  Prev();
+});
+
+// Esto hace que sea automático y es opcional
+setInterval(function() {
+  Next();
+}, 5000);
